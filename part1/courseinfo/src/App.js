930ff5014823c8d23d2fb2163ef14b5a0f1f2405
Yasir <code.yasir@gmail.com>
2022-02-24 10:02:06 +0300

    finish exercises 1.1 - 1.12

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,50 @@
+const Course = ({ courseName }) => <h1>{courseName}</h1>;
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+);
+
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => (
+  <div>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
+  </div>
+);
+const Total = ({ item1, item2, item3 }) => (
+  <p>Number of exercises: {item1 + item2 + item3}</p>
+);
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Course courseName={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total item1={exercises1} item2={exercises2} item3={exercises3} />
+    </div>
+  );
 };
+
 export default App;
